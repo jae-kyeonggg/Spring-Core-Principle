1fdd4ea0a5a042154f27800bd52fea70d2062a7f
jae-kyeonggg <hanjaekyung56@gmail.com>
2023-03-08 22:57:49 +0900

    order domain test

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -10,9 +10,11 @@ public class MemberServiceTest {
     void join() {
         //given
         Member member = new Member(1L,"memberA", Grade.VIP);
+
         //when
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
+
         //then
         Assertions.assertThat(member).isEqualTo(findMember);
     }
